57dcfaaae9aca9dfe7a4c7dfaa940a33f45974fe
Tahateber95 <mr.layto@gmail.com>
2024-11-05 11:16:01 +0100

    update export path

--- lib/itx_validator.dart
+++ lib/itx_validator.dart
@@ -3,4 +3,4 @@ library itx_validator;
 export 'package:itx_validator/src/schema/itx_schema.dart';
 export 'package:itx_validator/src/abstracts/schema_value.dart';
 export 'package:itx_validator/src/validators/itx_validator_builder.dart';
-export 'package:itx_validator/src/validators/types/common.dart';
+export 'package:itx_validator/src/validators/common.dart';
